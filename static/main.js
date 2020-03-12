854e58a355f8adb637594685827064297c43791a
Brandon <bmiller3822@gmail.com>
2020-03-12 18:51:26 -0400

    Trying a Googled bugfix.

--- static/main.js
+++ static/main.js
@@ -1,19 +1,19 @@
-function q(selector) {
-    return document.querySelector(selector)
-}
+// function q(selector) {
+//     return document.querySelector(selector)
+// }
 
-function main() {
-    noResults()
-}
+// function main() {
+//     noResults()
+// }
 
-function noResults() {
-    if snippets {
-    } else {
-        q(body).innerHTML = <p>Sorry, your search returned zero results.</p>
-    }
-}
+// function noResults() {
+//     if snippets {
+//     } else {
+//         q(body).innerHTML = <p>Sorry, your search returned zero results.</p>
+//     }
+// }
 
-document.addEventListener('DOMContentLoaded', function () {
-    main()
-})
+// document.addEventListener('DOMContentLoaded', function () {
+//     main()
+// })
 
