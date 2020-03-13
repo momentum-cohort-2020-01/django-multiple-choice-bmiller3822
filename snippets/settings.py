9ff55015105034727dff9955f917e62a4e4d6122
Brandon <bmiller3822@gmail.com>
2020-03-12 23:06:19 -0400

    Trying another Brian suggestion.

--- snippets/settings.py
+++ snippets/settings.py
@@ -23,7 +23,7 @@ environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / ...
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
