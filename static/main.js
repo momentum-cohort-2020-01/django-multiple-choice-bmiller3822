dd45d0998e7e140c947c3ca8a199e0a04bd6e2a2
Brandon <bmiller3822@gmail.com>
2020-03-17 19:42:05 -0400

    Trying to copy to clipboard again.

--- static/main.js
+++ static/main.js
@@ -1,32 +1,22 @@
-function q(selector) {
-    return document.querySelector(selector)
+function copy_to_clipboard() {
+    let buttons = document.querySelectorAll('clipboard');
+    for (let button of buttons) {
+        button.addEventListener('click', function (e) {
+            let parent = e.target.closest('#code-snippet')
+            let snippet = parent.querySelector('code')
+            let range = document.createRange()
+            range.selectNode(snippet)
+            window.getSelection().removeAllRanges()
+            window.getSelection().addRange(range)
+            try {
+                let successful = document.execCommand('copy')
+                let msg = successful ? 'successful' : 'unsuccessful'
+            } catch (err) {
+                console.log('Sorry, unable to copy snippet')
+            } window.getSelection().removeAllRanges()
+        })
+    }
 }
-
-
-function copyToClipboard() {
-    let clipboardButton = q("#clipboard")
-    let str = document.getElementById("#codearea").innerText
-    clipboardButton.addEventListener("click", function (event) {
-        event.preventDefault
-        const copyToClipboard = str => {
-            const el = document.createElement('textarea')
-            el.value = str
-            el.setAttribute('readonly', '')
-            el.style.position = 'absolute'
-            // el.style.left = '-9999px'
-            document.body.appendChild(el)
-            // const selected = document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false
-            // el.select()
-            document.execCommand('copy')
-            // document.body.removeChild(el)
-            // if (selected) {
-            //     document.getSelection().removeAllRanges()
-            //     document.getSelection().addRange(selected)
-            // }
-        }
-    })
-}
-
 
 function main() {
     copyToClipboard()
@@ -35,4 +25,64 @@ function main() {
 document.addEventListener('DOMContentLoaded', function () {
     main()
 })
+
+
+
+//code-snippet and codearea
+
+// function copy_to_clipboard() {
+//     let buttons = document.querySelectorAll('.fa-clipboard');
+//     for (let button of buttons) {
+//         button.addEventListener('click', function(event) {
+//             let parent = event.target.closest('.code-snippet');
+//             let snippet = parent.querySelector('code');
+//             let range = document.createRange();
+//             range.selectNode(snippet);
+//             window.getSelection().removeAllRanges();
+//             window.getSelection().addRange(range);
+//             try {
+//                 let successful = document.execCommand('copy');
+//                 let msg = successful ? 'successful' : 'unsuccessful';
+//                 console.log('Copy snippet command was ' + msg);
+//             } catch(err) {
+//                 console.log('Sorry, unable to copy snippet');
+//             } window.getSelection().removeAllRanges();
+//         })
+//     }   
+// }
+
+
+
+
+// function q(selector) {
+//     return document.querySelector(selector)
+// }
+
+
+// function copyToClipboard() {
+//     let clipboardButton = q("#clipboard")
+//     let str = document.getElementById("#codearea").innerText
+//     clipboardButton.addEventListener("click", function (event) {
+//         event.preventDefault
+//         const copyToClipboard = str => {
+//             const el = document.createElement('textarea')
+//             el.value = str
+//             el.setAttribute('readonly', '')
+//             el.style.position = 'absolute'
+//             // el.style.left = '-9999px'
+//             document.body.appendChild(el)
+//             // const selected = document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false
+//             // el.select()
+//             document.execCommand('copy')
+//             // document.body.removeChild(el)
+//             // if (selected) {
+//             //     document.getSelection().removeAllRanges()
+//             //     document.getSelection().addRange(selected)
+//             // }
+//         }
+//     })
+// }
+
+
+
 
